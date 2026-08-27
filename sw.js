e3105de8887f6c416d9c8ef19fa150d2ae173b28
Yassinebgm <yassine.benghanem@student.odisee.be>
2026-08-27 14:04:32 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,36 @@
-const CACHE_NAME = "hueco-mundo";
-const ASSETS = [
-    "./",
-    "./index.html",
-    "./style.css",
-    "./app.js",
-    "./manifest.json",
-    "./img/icon-192.png",
-    "./img/icon-512.png",
-    "./img/layered-waves-haikei.svg",
-    "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.min.js",
-    "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js"
+const CACHE_NAME = "hueco-mundo-v1.2.1";
+const LOCAL_ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./img/icon-192.png",
+  "./img/icon-512.png",
+  "./img/layered-waves-haikei.svg"
+];
+
+const CDN_ASSETS = [
+  "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js"
 ];
 
 self.addEventListener("install", (event) => {
-    event.waitUntil(
-        cashes.open(CACHE_NAME).then((cache) => cache.addALL(ASSETS))
-    );
-    self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(async (cache) => {
+      await cache.addAll(LOCAL_ASSETS);
+
+      for (const url of CDN_ASSETS) {
+        try {
+          const response = await fetch(url, { mode: "no-cors" });
+          await cache.put(url, response);
+        } catch (err) {
+          console.log("Failed to cache CDN asset:", url, err);
+        }
+      }
+    })
+  );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
